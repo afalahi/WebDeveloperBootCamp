9c72f4298403f4071a4e93f2eba46607a407dfc7
afalahi <ali@afalahi.com>
2017-11-17 15:22:48 -0500

    added new files

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
-var age = prompt("How old are you?");
-var days=age * 365;
-if (days > 0) {
-    alert(days)
-} else {
-    alert("You didn't enter a valid number")
+var age = prompt("How old are you");
+if (age < 18) {
+    alert("Sorry, we can't let you in")
+} else if(age < 21) {
+    alert("You can enter but cannot drink")
+}
+else{
+    if (age == 21) {
+        var bday="HAPPY BIRTHDAY!"
+        if (age%2!==0) {
+            var isOdd= "your age is odd!"
+        }
+        else{
+            isOdd="your age is not odd"
+        }
+        alert("You can enter and drink and "+ bday + " and "+isOdd)
+    }
+    else{
+        if (age%2!==0) {
+            var isOdd= "your age is odd!"
+        }
+        else{
+            isOdd="your age is not odd"
+        }
+        alert("You can enter and drink and " +isOdd)
+    }
 }
