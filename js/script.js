5930f9ba9c48015fad329fdf750211b665f85503
Ali <ali@afalahi.com>
2017-11-18 18:03:33 -0500

    js functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-var number=50;
-for (let index = 5; index <= number; index++) {
-    if (index%5===0&&index%3===0) {
-        console.log(index)
-    }
+function square(num) {
+    console.log(num*num)
 }
+square(10);
