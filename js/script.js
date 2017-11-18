1e59c15e84d13aaa4f8a5371eba3f512b07ad44d
Ali <ali@afalahi.com>
2017-11-17 19:15:42 -0500

    added number game

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 var number=prompt("Guess the number");
-if (number<2) {
-    alert("The number is too high")
-} else if(number>9) {
+if (number<=2) {
     alert("The number is too low")
+    console.log(number)
+} else if(number>=9) {
+    alert("The number is too high")
+}
+else if (number==7){
+    alert("You guessed right!")
+}
+else{
+    alert("Wrong number!")
 }
