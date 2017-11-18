fa1d4352126864ada1d2d3c0c97c4fef285978a7
Ali <ali@afalahi.com>
2017-11-18 14:05:03 -0500

    for loops

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-while (question!=="yeah"&&question!=="yes") {
-    var question=prompt("Are we there yet?");
+var number=50;
+for (let index = 5; index <= number; index++) {
+    if (index%5===0&&index%3===0) {
+        console.log(index)
+    }
 }
-alert("Yay, we finally made it");
