9e4b97e5129999ea0ec4c979b23fd4f77eb30cb0
Ali <ali@afalahi.com>
2017-11-19 23:55:11 -0500

    completed array iteration challenges

--- js/arrayiteration.js
+++ js/arrayiteration.js
@@ -7,4 +7,33 @@ function printReverse(value) {
   }
 };*/
 
-var numArray=[1,2,3,4];
+// var numArr=[1,1,1,1];
+// var charArr=[];
+// function isUniform(arr) {
+//     var baseIndex=arr[0];
+//     for (let index = 1; index < arr.length; index++) {
+//         if (arr[index]!==baseIndex) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// function sumArray(arr) {
+//     var sum=0
+//     for (let index = 0; index < arr.length; index++) {
+//         sum += arr[index]
+//     }
+//     return sum
+// }
+
+// function max(arr) {
+//     var max=arr[0];
+//     for (let index = 1; index < arr.length; index++) {
+//         if (max < arr[index]) {
+//             max=arr[index];
+//         }
+       
+//     }
+//     return max;
+// }
