56a01ce96b8d38c08e59871bc038bb2d228e1c41
Ali <ali@afalahi.com>
2017-11-19 22:46:51 -0500

    array iteration

--- js/arrayiteration.js
+++ js/arrayiteration.js
@@ -1,4 +1,10 @@
-var array=["red","green","blue"];
-array.forEach(function(arrayitem){
-    console.log(arrayitem)
-});
+/*var numArray=[1,2,3,4];
+var charArray=["A","B","C"]
+function printReverse(value) {
+  for (let index = value.length -1; index >=0; index--) {
+      console.log(value[index]);
+      
+  }
+};*/
+
+var numArray=[1,2,3,4];
