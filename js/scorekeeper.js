7abf9e68d51151db296f87df566130c8f1f02796
Ali Falahi <ali@afalahi.com>
2017-11-26 21:04:13 -0500

    finished score keeper

--- js/scorekeeper.js
+++ js/scorekeeper.js
@@ -8,24 +8,34 @@ var p2Points=0;
 var p2Score=document.querySelector("#p2-score")
 var gameOver=false;
 function addScore(player,points,score) {
-    var scoreLimit=document.querySelector("#score-limit").textContent
+    var scoreLimit=document.querySelector("#score-limit")
     player.addEventListener("click",function(){
         if(!gameOver){
             points++;            
-            if(Number(scoreLimit)===points){
+            if(Number(scoreLimit.textContent)===points){
                 gameOver=true
                 score.classList.add("green")
             }
             score.textContent=points
         }
     });
-    var reset=document.querySelector("#reset");
-    reset.addEventListener("click",function(){
-    score.textContent="0";
-    points=0;
-    score.classList.remove("green");
-    gameOver=false;
+    //Update score limit
+    var updateLimit=document.querySelector("input");
+    updateLimit.addEventListener("change",function() {
+    scoreLimit.innerHTML=updateLimit.value;
+    reset();
     });
+    //Reset the game
+    var resetGame=document.querySelector("#reset");
+    resetGame.addEventListener("click",function(){
+    reset();
+    });
+    function reset() {
+        score.textContent="0";
+        points=0;
+        score.classList.remove("green");
+        gameOver=false;
+    }
 }
 addScore(playerOne,p1Points,p1Score);
 addScore(playerTwo,p2Points,p2Score);
