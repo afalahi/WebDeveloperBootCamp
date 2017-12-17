36f799c299cefcdbb1d8078b6a2f34c28ed8ddc6
Ali Falahi <ali@afalahi.com>
2017-12-17 15:20:36 -0500

    finishing color game

--- js/ColorGame/colorgame.js
+++ js/ColorGame/colorgame.js
@@ -1,9 +1,13 @@
 //GLOBAL VARS
-var colors=colorGenerator(6);
+var numSquares=6;
+var colors=colorGenerator(numSquares);
 var squares=document.querySelectorAll(".square");
 var pickedColor=colorPicker();
 var colorDisplay= document.querySelector("#colorDisplay");
 var messageDisplay=document.querySelector(".message");
+var resetButton=document.querySelector("#reset");
+var easy=document.querySelector("#easy");
+var hard=document.querySelector("#hard");
 //Functions
 //Color Changer
 function changeColors(color) {
@@ -40,12 +44,50 @@ for (let i = 0; i < squares.length; i++) {
         var clickedColor=this.style.backgroundColor;
         if (clickedColor===pickedColor) {
             messageDisplay.textContent="Correct!";
+            resetButton.textContent="Play Again"
             changeColors(clickedColor);
             document.querySelector("#header").style.backgroundColor=clickedColor;
         }
         else{
-            this.style.backgroundColor="#232323";
+            this.style.backgroundColor="steelblue";
             messageDisplay.textContent="Try Again!";
         }
     });
 }
+resetButton.addEventListener("click",function(){
+    colors=colorGenerator(numSquares);
+    pickedColor=colorPicker();
+    colorDisplay.textContent=pickedColor;
+    for (let i = 0; i < squares.length; i++) {
+        //sets initial colors
+        squares[i].style.backgroundColor=colors[i];
+    }
+    document.querySelector("#header").style.backgroundColor="steelblue";
+});
+
+easy.addEventListener("click", function () {
+    numSquares=3;
+    hard.classList.remove("selected");
+    easy.classList.add("selected");
+    colors=colorGenerator(numSquares);
+    pickedColor=colorPicker();
+    colorDisplay.textContent=pickedColor;
+    for (let index = 0; index < squares.length; index++) {
+        if(colors[index]){
+            squares[index].style.backgroundColor=colors[index];
+        } else{
+            squares[index].style.display="none";
+        }
+    }
+});
+hard.addEventListener("click", function () {
+    numSquares=6
+    easy.classList.remove("selected");
+    hard.classList.add("selected");
+    colors=colorGenerator(numSquares);
+    pickedColor=colorPicker();
+    for (let index = 0; index < squares.length; index++) {
+            squares[index].style.backgroundColor=colors[index];
+            squares[index].style.display="block";
+    }
+});
