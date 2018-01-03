ad9813c023be4538b72e681aef20d2c2d484b694
Ali Falahi <ali@afalahi.com>
2018-01-02 21:48:01 -0500

    update

--- IntroToNode/grader.js
+++ IntroToNode/grader.js
@@ -1,11 +1,10 @@
 function average(arr){
     var sum=0;
     for (var i = 0; i < arr.length; i++) {
-        
         sum = sum + arr[i];
     }
     sum = sum / arr.length;
-    console.log(Math.round(sum));
+    return Math.round(sum);
 }
 var scores = [90, 98, 89, 100, 100, 86, 94];
-average(scores);
+console.log(average(scores));
