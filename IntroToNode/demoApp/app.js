8a21385e5645dcf676c1f7de9adf798192a0ee27
Ali Falahi <ali@afalahi.com>
2018-01-02 23:02:00 -0500

    added modules

--- IntroToNode/demoApp/app.js
+++ IntroToNode/demoApp/app.js
@@ -1,6 +1,1 @@
-var faker = require("faker");
-for (let i = 0; i < 10; i++) {
-    var productName = faker.commerce.productName();
-    var productPrice = faker.commerce.price();
-    console.log(productName+" - "+"$"+productPrice);
-}
+/*jshint esversion: 6 */
