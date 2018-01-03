72235ab10470fbcc18f02648dc80a650867a6f47
Ali Falahi <ali@afalahi.com>
2018-01-03 00:27:55 -0500

    more modules

--- IntroToNode/demoApp/app.js
+++ IntroToNode/demoApp/app.js
@@ -1,3 +1,10 @@
 /*jshint esversion: 6 */
 var express = require("express");
-var app = express;
+var app = express();
+app.get("/", function(req, res){
+    res.send("Hi there");
+});
+app.get("/bye", function(req, res){
+    res.send("good bye");
+})
+app.listen(8080);
