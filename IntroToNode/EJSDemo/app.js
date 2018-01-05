4c54508d113bbe7466a523c848d7b290b81c88c6
Ali Falahi <ali@afalahi.com>
2018-01-05 01:14:54 -0500

    added post requests

--- IntroToNode/EJSDemo/app.js
+++ IntroToNode/EJSDemo/app.js
@@ -3,11 +3,13 @@
 const path = require ('path');
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '/public')));
 app.set('views', path.join(__dirname, '/views'));
 app.set("view engine", "ejs");
-
+var friends=["Ali", "Mike", "Russ", "Edgar", "Tamir"];
 //routs
 app.get("/", function(req, res){
     res.render("home");
@@ -35,6 +37,16 @@ app.get("/posts", function (req, res) {
         }
     ];
     res.render("posts",{posts: posts});
+});
+
+app.post("/addfriend", function(req, res){
+    var friend = req.body.friend;
+    friends.push(friend);
+    res.redirect("/friends");
+});
+
+app.get("/friends", function(req, res){
+    res.render("friends", {friends: friends});
 });
 
 //server
