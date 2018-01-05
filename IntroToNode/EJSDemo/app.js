39580e9993d281d84849a1291c2e81cb0d6b237c
Ali Falahi <ali@afalahi.com>
2018-01-05 10:59:45 -0500

    add

--- IntroToNode/EJSDemo/app.js
+++ IntroToNode/EJSDemo/app.js
@@ -6,6 +6,7 @@ const app = express();
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/public')));
 app.set('views', path.join(__dirname, '/views'));
 app.set("view engine", "ejs");
