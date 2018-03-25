466bac3bd65f4af5a53f93ed6facc2a92f5daee7
Ali Falahi <ali@afalahi.com>
2018-03-25 16:26:57 -0400

    last test

--- IntroToNode/blog/app.js
+++ IntroToNode/blog/app.js
@@ -7,7 +7,6 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const sanitizer = require('express-sanitizer');
-
 const db = require('./config/db');
 
 const app = express();
@@ -20,7 +19,7 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride("_method"));
