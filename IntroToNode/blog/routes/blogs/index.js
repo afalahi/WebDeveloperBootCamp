0a54d3d8de729e384efbe8a1e9d5b8b9ea904044
Ali Falahi <ali@afalahi.com>
2018-03-25 11:58:57 -0400

    added view, edit, and delete routes + multer support

--- IntroToNode/blog/routes/blogs/index.js
+++ IntroToNode/blog/routes/blogs/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const blog = require('../../models/blog');
-
+const multer = require('multer');
+//MULTER CONFIG
+const storage = multer.diskStorage(
+	{  
+		destination: function (req, file, cb) {
+			cb(null, './public/images')
+		},
+		filename: function(req, file, callback) {
+			callback(null,Date.now()+'-'+file.originalname);
+		}
+	}
+);
+const upload = multer({storage:storage});
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
 	res.redirect("/blogs");
 });
 //Get Blogs
-router.get("/blogs", function(req,res,next){
+router.get("/blogs", function(req,res){
 	blog.find({}, 
 		function(err,result){
 			if(err){
@@ -20,11 +32,14 @@ router.get("/blogs", function(req,res,next){
 	);
 });
 //NEW Blog
-router.get("/blogs/new", function(req,res,next){
+router.get("/blogs/new", function(req,res){
     res.render("new", {title: "New Blog"})
 });
 //Create blog
-router.post("/blogs", function(req, res, next){
+router.post("/blogs", upload.single('image') , function(req, res){
+	req.body.blog.title = req.sanitize(req.body.blog.title);
+	req.body.blog.body = req.sanitize(req.body.blog.body);
+	req.body.blog.image = '/images/'+ req.file.filename
 	blog.create(req.body.blog, function(err, newBlog){
 		if(err){
 			res.render("new", {title: "New Blog"})
@@ -32,5 +47,58 @@ router.post("/blogs", function(req, res, next){
 			res.redirect("/blogs");
 		}
 	});
+});
+// VIEW BLOG
+router.get("/blogs/:id", function(req, res){
+	let id = req.params.id;
+	blog.findById(id, function(err, result){
+		if(err){
+			res.render("/");
+		}
+		else{
+			res.render('view', {
+				title:result.title,
+				blog: result
+			});
+		}
+	});
+});
+// EDIT ROUTE
+router.get("/blogs/:id/edit", function(req, res){
+	blog.findById(req.params.id, function(err, result){
+		if(err){
+			res.redirect("/blogs");
+		} else{
+			res.render('edit', {
+				title:result.title,
+				blog:result
+			});
+		}
+	});
+});
+//UPDATE ROUTE
+router.put("/blogs/:id", upload.single("image"), function(req, res){
+	req.body.blog.title = req.sanitize(req.body.blog.title);
+	req.body.blog.body = req.sanitize(req.body.blog.body);
+	if(req.file){
+		req.body.blog.image = '/images/'+ req.file.filename
+	}
+	blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, result){
+		if (err) {
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs/" + req.params.id);
+		}
+	});
+});
+// DELETE ROUTE
+router.delete("/blogs/:id", function(req, res){
+	blog.findByIdAndRemove(req.params.id, function(err){
+		if (err) {
+			res.redirect("/blogs/" + req.params.id);
+		} else {
+			res.redirect("/blogs")
+		}
+	})
 });
 module.exports = router;
