eff95a47d8990488f7c39d0887fc778b6fd6e7ad
Ali Falahi <ali@afalahi.com>
2018-04-16 00:41:03 -0400

    changed the env var to one value

--- IntroToNode/YelpCamp/config/database.js
+++ IntroToNode/YelpCamp/config/database.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
-const dbUri = process.env.MLAB_URI,
-      dbUser = process.env.DB_USER,
-      dbPass=process.env.DB_PASS
-mongoose.connect('mongodb://'+dbUser+':'+dbPass+'@'+dbUri+'/yelpcamp', function(err){
+mongoose.connect(`mongodb://${process.env.MONGO_DB_URI}/yelpcamp`, function(err){
     if(err){
         throw err.message;
     }
